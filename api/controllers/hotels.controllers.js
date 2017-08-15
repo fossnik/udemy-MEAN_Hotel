ffc109f228d3e7b8422c881b5dce9efb4c750fed
Zack Hartmann <zackhartmann@gmail.com>
2017-08-15 13:39:55 -0400

    Lecture 22 Completed

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,14 +1,11 @@
 var dbconn = require('../data/dbconnection.js');
+var ObjectId = require('mongodb').ObjectId;
 var hotelData = require('../data/hotel-data.json');
 
 module.exports.hotelsGetAll = function(req, res) {
 
 	var db = dbconn.get();
-
-	console.log("db", db);
-	
-	console.log("GET the json");
-	console.log(req.query);
+	var collection = db.collection('hotels');
 
 	var offset = 0;
 	var count = 5;
@@ -21,21 +18,34 @@ module.exports.hotelsGetAll = function(req, res) {
 		count = parseInt(req.query.count, 10);
 	}
 
-	var returnData = hotelData.slice(offset, offset+count);
-
-	res
-		.status(200)
-		.json( returnData );
+	collection
+		.find()
+		.skip(offset)
+		.limit(count)
+		.toArray(function(err, docs) {
+			console.log("Found hotels", docs);
+			res
+				.status(200)
+				.json( docs );
+		});
 };
+
 
 module.exports.hotelsGetOne = function(req, res) {
+	var db = dbconn.get();
+	var collection = db.collection('hotels');
+
 	var hotelId = req.params.hotelId;
-	var thisHotel = hotelData(hotelId);
 	console.log("GET the hotelId", hotelId);
-	res
-		.status(200)
-		.json( thisHotel );
+
+	collection
+		.findOne({}, function(err, doc) {
+			res
+				.status(200)
+				.json( doc );
+		});
 };
+
 
 module.exports.hotelsAddOne = function(req, res) {
 	console.log("POST new hotel");
